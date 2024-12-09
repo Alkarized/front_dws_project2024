7d36499822d1b9fb806926718b646b38c5457a1e
Mikhail Oskilko <311707@niuitmo.ru>
2024-12-09 20:09:56 +0300

    deploy

--- src/components/segments/table/SegmentTable.tsx
+++ src/components/segments/table/SegmentTable.tsx
@@ -13,122 +13,61 @@ import LoadingProgressTable from "../components/LoadingProgressTable";
 import {
     addNewSegment,
     deleteSegments, filters,
-    getSegmentsOnPage,
+    getSegmentsOnPage, getSegmentsOnPageWithFilter, ReturnData,
     START_LENGTH,
     updateSegmentData
 } from "../../../api/SegmentApi";
 import {notifyError, notifyLoading, updateError, updateSuccess} from "../../../toast/Notifies";
 import {Id} from "react-toastify/dist/types";
 import SegmentAdd from "../components/SegmentAdd";
-import DisterbuteComponent from "../components/DisterbuteComponent";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import {Simulate} from "react-dom/test-utils";
+import select = Simulate.select;
+import copy = Simulate.copy;
+import DistributeSegment from "../components/DistributeSegment";
+
+const createEmptyNumberSet = (): Set<Id> => {
+    return new Set<Id>();
+}
+
+const createNumberSet = (set: Set<Id>) => {
+    return new Set<Id>(set);
+}
 
 export default function SegmentTable() {
-    const [selected, setSelected] = useState<number[]>([]);
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
     const [visible, setVisible] = useState([])
     const [isLoading, setIsLoading] = useState(false)
-    const [nid, setNid] = useState<Id>(0);
-    const [textToDisplay, setTextToDisplay] = useState("")
+    const [selected, setSelected] = useState<Set<Id>>(createEmptyNumberSet());
     const [selectedAll, setSelectedAll] = useState(false)
-    const [pageChanged, setPageChanged] = useState(false)
     const [emptyRows, setEmptyRows] = useState(0)
     const [segmentAddValue, setSegmentAddValue] = useState("");
     const [isProcessAdd, setIsProcessAdd] = useState(false)
     const [rowsAmount, setRowsAmount] = useState(0)
     const [filterElements, setFilterElements] = useState<filters>({nameFilter: "", idFilter: ""})
+    const [prevFilterElements, setPrevFilterElements] = useState<filters>({nameFilter: "", idFilter: ""})
+    const [rerender, setRerender] = useState(false);
+    const [isFilterSet, setIsFilterSet] = useState(false);
+    const [pageChanged, setPageChanged] = useState(false);
+
+    const checkFilterSet = () : boolean => {
+        return filterElements.idFilter != "" || filterElements.nameFilter != "";
+    }
+
+
+    const toggleRerender = () => {
+        setRerender((prev) => !prev)
+    }
 
     const handleSegmentAddValue = (e) => {
         setSegmentAddValue(e.target.value.trim())
     }
 
-    const handleAddSegment = () => {
-        if (segmentAddValue === ""){
-            notifyError("Пустое поле")
-            return
-        }
-
-        setIsProcessAdd(true);
-
-        const id = notifyLoading("Добавление элемента");
-
-        addNewSegment(segmentAddValue).then((e : number)=>{
-            updateSuccess(id, "Элемент добавлен")
-            setRowsAmount(amount=> amount + e)
-        }).finally(()=>{
-            setIsProcessAdd(false);
-        })
-    }
-
-    const clearSelections = () => {
-        setSelectedAll(false)
-        setSelected([])
-        setPageChanged(false)
-    }
-
-    const setSelAll = () => {
-        setSelectedAll(true)
-        setSelected([])
-    }
-
-    const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => { //todo FIX
-        const newSelected = visible.map((n) => n.id);
-        //console.log(newSelected + " " + selectedAll + " " + rowsAmount + " " + selected)
-
-        if (selectedAll) {
-            if (selected.length == 0){
-                setSelectedAll(false)
-            }
-            setSelected([])
-        } else {
-            if (selected.length != 0 && selected.length % rowsPerPage == 0 || selected.length == rowsAmount){
-                if (pageChanged){
-                    setSelected(sels => sels.concat(newSelected))
-                    setPageChanged(false)
-                } else {
-                    setSelAll()
-                }
-                return
-            }
-            if (selected.length > rowsPerPage){
-                setSelected(sels => sels.concat(newSelected))
-            } else {
-                if (newSelected.length == rowsAmount) {
-                    setSelAll()
-                } else {
-                    setSelected(newSelected)
-                }
-
-            }
-
-        }
-
-    };
-
-    //todo add filters!
-
-    const handleClick = (event: React.MouseEvent<unknown>, id: number) => { //todo fix bug when selected <= rowsAmount and page == 1 + check selectedAll
-        const selectedIndex = selected.indexOf(id);
-        let newSelected: number[] = [];
-
-        if (selectedIndex === -1) {
-            newSelected = newSelected.concat(selected, id);
-        } else if (selectedIndex === 0) {
-            newSelected = newSelected.concat(selected.slice(1));
-        } else if (selectedIndex === selected.length - 1) {
-            newSelected = newSelected.concat(selected.slice(0, -1));
-        } else if (selectedIndex > 0) {
-            newSelected = newSelected.concat(
-                selected.slice(0, selectedIndex),
-                selected.slice(selectedIndex + 1),
-            );
-        }
-        setSelected(newSelected);
-    };
-
     const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
-        setPageChanged(true)
     };
 
     const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -136,93 +75,213 @@ export default function SegmentTable() {
         setPage(0);
     };
 
-    const handleFilter = (value : string, label : string) => { //todo fix with filters pagination! + fix chekced all
-        if (label == "ID"){
-            setFilterElements(elem => {
-                return {
-                    ...elem,
-                    idFilter: value
-                }
-            })
-        } else if (label == "NAME"){
-            setFilterElements(elem => {
-                return {
-                    ...elem,
-                    nameFilter: value
-                }
-            })
-        }
+    const setSelAll = () => {
+        setSelectedAll(true)
+        setSelected(createEmptyNumberSet())
     }
 
+    const addToSelected = (toAdd) => {
+        setSelected(prev => {
+            toAdd.forEach(e => {
+                prev.add(e)
+            })
+            return createNumberSet(prev);
+        })
+    }
+
+    const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => { //todo FIX
+        const pageSelections = visible.map((n) => n.id);
+
+        let isAllOnPageSelected = true;
+        let toAdd : Id[] = []
+
+        pageSelections.forEach(e => {
+            if (!selected.has(e)) {
+                isAllOnPageSelected = false;
+                toAdd.push(e);
+            }
+        })
+
+/*        if (isFilterSet){
+
+            addToSelected(toAdd);
+            return;
+        }*/
+
+        if (selectedAll || rowsAmount == selected.size){
+            if (selected.size == 0){
+                setSelectedAll(false)
+            }
+            setSelected(createEmptyNumberSet())
+        } else {
+            if (isAllOnPageSelected) {
+                setSelAll()
+                return
+            }
+
+            addToSelected(toAdd);
+
+        }
+
+    };
+
+    //todo add filters!
+
+    const handleSelect = (event: React.MouseEvent<unknown>, id: number) => { //todo fix bug when selected <= rowsAmount and page == 1 + check selectedAll
+        if (selected.has(id)) {
+            setSelected(prev => {
+                prev.delete(id);
+                return createNumberSet(prev);
+            })
+        } else {
+            setSelected(prev => {
+                prev.add(id);
+                return createNumberSet(prev);
+            })
+        }
+    };
+
+    const handleFilter = (value: string, label: string) => { //todo fix with filters pagination! + fix chekced all
+        let trimValue = value.trim();
+
+        if (label == "ID") {
+            if (prevFilterElements.idFilter != trimValue) {
+
+                setFilterElements(elem => {
+                    return {
+                        ...elem,
+                        idFilter: trimValue
+                    }
+                })
+
+                setPrevFilterElements(elem => {
+                    return {
+                        ...elem,
+                        idFilter: trimValue
+                    }
+                })
+
+            } else {
+                return
+            }
+
+        } else if (label == "NAME") {
+            if (prevFilterElements.nameFilter != trimValue){
+                setPrevFilterElements(elem => {
+                    return {
+                        ...elem,
+                        nameFilter: trimValue
+                    }
+                })
+
+                setFilterElements(elem => {
+                    return {
+                        ...elem,
+                        nameFilter: trimValue
+                    }
+                })
+            } else {
+                return;
+            }
+
+        }
+
+        toggleRerender()
+    }
 
     const handleChangeById = (id, newValue) => {
         setIsLoading(true)
 
         let tmpId = notifyLoading("Обновление значения элемента")
-        setTextToDisplay("Значение элемента обновлено")
 
-        updateSegmentData(id, newValue).then(()=>{
-            setNid(tmpId)
+        updateSegmentData(id, newValue).then(() => {
+            updateSuccess(tmpId, "Значение элемента обновлено");
+
+            toggleRerender()
         })
 
     }
 
-    const handleDeleteSegments = () => {
+    const handleDeleteSegments = () => {  //todo FIX bug если я оставлю единственный элемент на странице!!!
         setIsLoading(true)
-        clearSelections()
-
         let tmpId = notifyLoading("Элементы удаляются")
 
-        deleteSegments(selectedAll, selected).then((deletedAmount : number)=> {
-            setSelected([])
+        deleteSegments(selectedAll, selected).then(() => {
             updateSuccess(tmpId, "Выбранные элементы удалены")
-            let diff = rowsAmount - deletedAmount
-            let amount = ~~(diff / rowsPerPage)
 
-            if (deletedAmount == 0){
-                setIsLoading(false)
-            }
+            setSelected(createEmptyNumberSet());
+            setSelectedAll(false);
 
-            setRowsAmount(diff)
-
-            if (page >= amount) { //todo FIX bug если я оставлю единственный элемент на странице!!!
-                setPage(diff % rowsPerPage == 0 ? Math.max(0, amount - 1) : amount)
-            }
-
+            toggleRerender();
         })
 
     }
 
+    const handleAddSegment = () => {
+        if (segmentAddValue === "") {
+            notifyError("Пустое поле")
+            return
+        }
 
-    useEffect(() => {
-        setRowsAmount(START_LENGTH)
-    }, []);
-
-
-    useEffect(() => {
-        getSegmentsOnPage(rowsPerPage, page, filterElements).then((r : any[]) => {
-            setVisible(r)
-            updateSuccess(nid, textToDisplay);
-            setIsLoading(false)
-        }).catch(e => {
-            updateError(nid, "Ошибка сервера");
-        })
-
-    }, [nid])
-
-    useEffect(() => {
+        setIsProcessAdd(true);
         setIsLoading(true)
-        setEmptyRows(Math.max(0, (1 + page) * rowsPerPage - rowsAmount))
 
-        getSegmentsOnPage(rowsPerPage, page, filterElements).then((r : any[]) => {
-            setVisible(r)
-            setIsLoading(false)
+        const id = notifyLoading("Добавление элемента");
+
+        addNewSegment(segmentAddValue).then((e: number) => {
+            updateSuccess(id, "Элемент добавлен")
+            if (selectedAll){
+                setSelected(prev=>{
+                    prev.add(e)
+                    return prev;
+                })
+            }
+        }).finally(() => {
+            toggleRerender()
+
+            setIsProcessAdd(false);
+        })
+    }
+
+    useEffect(() => {
+        setIsFilterSet(checkFilterSet())
+    }, [filterElements]);
+
+    useEffect(() => {
+        if (pageChanged){
+            setPageChanged(false)
+            return
+        }
+
+        setIsLoading(true)
+
+        getSegmentsOnPageWithFilter(rowsPerPage, page, filterElements).then((r: ReturnData) => {
+            setVisible(r.data)
+            setRowsAmount(r.rows)
+
+            if (page * rowsPerPage >= r.rows && r.rows != 0) {
+                let diff = ~~(r.rows / rowsPerPage);
+                setPage(r.rows % rowsPerPage == 0 ? Math.max(0, diff - 1) : diff)
+            } else {
+
+                setIsLoading(false)
+
+                if (r.rows == 0 && page != 0) {
+                    setPage(0)
+                    setPageChanged(true);
+                    setIsLoading(false)
+                }
+            }
+
+            setEmptyRows(Math.max(0, (1 + page) * rowsPerPage - r.rows))
 
         }).catch(e => {
             notifyError("Ошибка получения данных с сервера")
+        }).finally(() => {
+
         })
 
-    }, [page, rowsPerPage, rowsAmount, setRowsAmount, filterElements])
+    }, [page, rowsPerPage, rerender])
 
     return (
         <>
@@ -233,12 +292,12 @@ export default function SegmentTable() {
                 isProcessAdd={isProcessAdd}
 
             />
-            <DisterbuteComponent
+            <DistributeSegment
             />
             <Box sx={{width: '100%'}}>
                 <Paper sx={{width: '100%', mb: 2}}>
                     <SegmentTableToolBar
-                        numSelected={selectedAll ? rowsAmount - selected.length : selected.length}
+                        numSelected={selectedAll ? rowsAmount - selected.size : selected.size}
                         handleDeleteSegments={handleDeleteSegments}
                     />
                     <TableContainer>
@@ -248,27 +307,32 @@ export default function SegmentTable() {
                             size={'medium'}
                         >
                             <SegmentTableHead
-                                numSelected={selectedAll ? rowsAmount - selected.length : selected.length}
+                                numSelected={selectedAll ? rowsAmount - selected.size : selected.size}
                                 onSelectAllClick={handleSelectAllClick}
                                 rowCount={rowsAmount}
                                 handleFilter={handleFilter}
+                                isAllSelected={selectedAll}
                             />
                             {isLoading ? (
                                 <LoadingProgressTable rowsPerPage={rowsPerPage}/>
                             ) : visible.length == 0 ? (
-                                    <div>
-                                        Nothing to see
-                                    </div>
-                                ) : (
-                                    <SegmentTableBody
-                                        allSelected={selectedAll}
-                                        visibleRows={visible}
-                                        selected={selected}
-                                        handleClick={handleClick}
-                                        emptyRows={emptyRows}
-                                        handleChangeById={handleChangeById}
-                                    />
-                                )}
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>
+                                            Nothing to see
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            ) : (
+                                <SegmentTableBody
+                                    allSelected={selectedAll}
+                                    visibleRows={visible}
+                                    selected={selected}
+                                    handleClick={handleSelect}
+                                    emptyRows={emptyRows}
+                                    handleChangeById={handleChangeById}
+                                />
+                            )}
 
                         </Table>
                     </TableContainer>
